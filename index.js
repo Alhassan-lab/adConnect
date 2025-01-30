fd592a3427d1d1306f8e0df268ce39147726ebbe
Abdullahi Alhassan <129526588+Alhassan-lab@users.noreply.github.com>
2025-01-30 21:46:15 +0000

    recent adjustment

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get all buttons with the class "btn" and add a click event listener
-    const buttons = document.querySelectorAll(".btn");
-    
-    buttons.forEach(function (button) {
+  const buttons = document.querySelectorAll(".btn");
+  
+  buttons.forEach(function (button) {
       button.addEventListener("click", function () {
-        // Log a message to the console when a button is clicked
-        console.log(`Button clicked: ${button.textContent}`);
-        
-        // You can add more functionality here based on your requirements
+          console.log(`Button clicked: ${button.textContent}`);
+          // Add more functionality here
       });
-    });
   });
-
+});
